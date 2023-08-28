5be4ad31389ead6c4df4ab16ef16be798a628e2d
Huiying Shen <hshen@ski.org>
2023-08-28 16:32:56 -0700

    pyscript getting cv2 image from canvas working!

---
+++ static/homography.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class Homography:
+    def findId(self,id):
+        for i in range(len(self.ids)):
+            if id[0] == self.ids[i][0]:
+                return i
+        return -1
+    
+    def getCornerPairs(self,corners, ids):
+        if ids is None: return
+        vCn1,vCn2 = [],[]
+        for i,id in enumerate(ids):
+            indx = self.findId(id)
+            if indx != -1:
+                vCn1.extend(corners[i][0])
+                vCn2.extend(self.corners[indx][0])
+        return vCn1,vCn2
+        
+    def getHomography(self,vCn1,vCn2):
+        assert len(vCn1) == len(vCn2) and len(vCn1) >= 4
+        A = []
+        for i in range(len(vCn1)):
+            x, y = vCn1[i]
+            u, v = vCn2[i]
+            A.extend([
+                [-x, -y, -1, 0, 0, 0, u*x, u*y, u],
+                [0, 0, 0, -x, -y, -1, v*x, v*y, v]
+            ])
+        A = np.array(A)
+        
+        _, _, V = np.linalg.svd(A)
+        self.h = V[-1].reshape(3, 3)
+        # print("getHomography(): \n",self.h)
+
+    def verify(self,vCn1,vCn2):
+         for i in range(len(vCn1)):
+            x, y = vCn1[i]
+            u1,v1 = self.warp_func(x,y)
+            u, v = vCn2[i]
+            print(f'{u:5.1f} {v:5.1f} {u1:5.1f} {v1:5.1f} ') 
+
+    def warp_func(self,x,y):
+        xy_homogeneous = np.array([x, y, 1])
+        uvw = np.dot(self.h, xy_homogeneous)
+        return [uvw[0] / uvw[2], uvw[1] / uvw[2]]
--- static/main.py
+++ static/main.py
@@ -1,50 +1,18 @@
 import numpy as np
 import cv2
 import cv2.aruco as aruco
+from homography import Homography
 
-class Homography:
+class HomographyCv(Homography):
     def loadImageTest(self):
         self.im0 = cv2.imread("market_tmap.png")
         self.corners, self.ids, _ = aruco.detectMarkers(self.im0,aruco.Dictionary_get(cv2.aruco.DICT_4X4_250))
         return self
-    
-    def findId(self,id):
-        for i in range(len(self.ids)):
-            if id[0] == self.ids[i][0]:
-                return i
-        return -1
-    
+
     def getCornersTest(self):
         self.im = cv2.imread("marker_test.jpg")
         corners, ids, _ = aruco.detectMarkers(self.im,aruco.Dictionary_get(cv2.aruco.DICT_4X4_250))
         return corners, ids
-    
-    def getCornerPairs(self,corners, ids):
-        if ids is None: return
-        vCn1,vCn2 = [],[]
-        for i,id in enumerate(ids):
-            indx = self.findId(id)
-            if indx != -1:
-                # print(self.ids[indx][0])
-                vCn1.extend(corners[i][0])
-                vCn2.extend(self.corners[indx][0])
-        # self.drawMarker(self.im0,vCn2)
-        # cv2.imshow("im0",self.im0)
-        # self.drawMarker(self.im,vCn1)
-        # cv2.imshow("im",self.im)
-        return vCn1,vCn2
-
-    def verify(self,vCn1,vCn2):
-         for i in range(len(vCn1)):
-            x, y = vCn1[i]
-            u1,v1 = self.warp_func(x,y)
-            u, v = vCn2[i]
-            print(f'{u:.1f} {v:.1f} {u1:.2f} {v1:.2f} ') 
-
-    def warp_func(self,x,y):
-        xy_homogeneous = np.array([x, y, 1])
-        uvw = np.dot(self.h, xy_homogeneous)
-        return [uvw[0] / uvw[2], uvw[1] / uvw[2]]
     
     def drawMarker(self,bgr,corners):
         assert len(corners)%4 == 0
@@ -61,25 +29,7 @@ class Homography:
     
     def getHomographyCv(self,vCn1,vCn2):
         self.h, status = cv2.findHomography(np.array(vCn1),np.array(vCn2))
-        print("getHomographyCv(): \n",self.h)
-
-    def getHomography(self,vCn1,vCn2):
-        # Check that there are at least 4 corresponding points
-        assert len(vCn1) == len(vCn2) and len(vCn1) >= 4
-        
-        A = []
-        for i in range(len(vCn1)):
-            x, y = vCn1[i]
-            u, v = vCn2[i]
-            A.extend([
-                [-x, -y, -1, 0, 0, 0, u*x, u*y, u],
-                [0, 0, 0, -x, -y, -1, v*x, v*y, v]
-            ])
-        A = np.array(A)
-        
-        _, _, V = np.linalg.svd(A)
-        self.h = V[-1].reshape(3, 3)
-        print("getHomography(): \n",self.h)
+        # print("getHomographyCv(): \n",self.h)
 
     def test0(self):
         self.loadImageTest()
@@ -93,17 +43,19 @@ class Homography:
         cv2.imshow("im",self.im)
         self.getHomographyCv(vCn1,vCn2 )
         cv2.waitKey(0)
-        
-
 
     def test1(self):
 
         self.loadImageTest()
+        cv2.imshow("im0",self.im0)
         cap = cv2.VideoCapture(0)
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
         while True:
             cv2.waitKey(10)
             ret, self.im = cap.read()
             corners, ids, _ = aruco.detectMarkers(self.im,aruco.Dictionary_get(cv2.aruco.DICT_4X4_250))
+            cv2.imshow("im",self.im)
             if ids is None or len(ids) < 3: continue
             vCn1,vCn2 = self.getCornerPairs(corners,ids)
             im0 = self.im0.copy()
@@ -123,5 +75,5 @@ class Homography:
 
 
 if __name__ == '__main__':
-    Homography().test1()
+    HomographyCv().test1()
 
