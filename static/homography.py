5a75cb9a42b92d2b750e929f42f619c97bf9241f
Huiying Shen <hshen@ski.org>
2023-09-14 00:28:37 -0700

    pyscript imshow works

--- static/homography.py
+++ static/homography.py
@@ -1,4 +1,16 @@
+import base64
 import numpy as np
+import cv2
+import cv2.aruco as aruco
+
+def data_uri_to_cv2_img(data_uri):
+    data_uri += "=" * ((4 - len(data_uri) % 4) % 4) # Base64 needs a string with length multiple of 4. If the string is short, it is padded with 1 to 3 =.
+    # print("data_uri[:100] = ",data_uri[:100])
+    nparr = np.frombuffer(base64.b64decode(data_uri), np.uint8)
+    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    print(img.shape)
+    return img
+
 class Homography:
     def __init__(self):
         self.ids = []
@@ -47,3 +59,51 @@ class Homography:
         xy_homogeneous = np.array([x, y, 1])
         uvw = np.dot(self.h, xy_homogeneous)
         return [uvw[0] / uvw[2], uvw[1] / uvw[2]]
+
+class HomographyCv(Homography):
+    def __init__(self):
+        super().__init__()
+        self.im0 = None
+        self.im = None
+        #print(dir(aruco))
+        self.detector = aruco.ArucoDetector(aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250))
+
+    def loadMapImage(self,im0):
+        self.im0 = im0
+        self.corners, self.ids, _ = self.detector.detectMarkers(self.im0)
+
+
+    def loadMapDataUri(self,dataURL):
+        self.loadMapImage(data_uri_to_cv2_img(dataURL))
+
+
+    def tryGetHomography(self,im):
+        self.im = im
+        corners, ids, _ = self.detector.detectMarkers(self.im)
+        if ids is None or len(ids) < 3: return False
+        vCn1,vCn2 = self.getCornerPairs(corners,ids)
+        self.drawMarker(self.im,vCn1)
+        if len(vCn1) < 12: return False
+        self.getHomography(vCn1,vCn2)
+        return True
+    
+    def tryGetHomographyDataUri(self,testDataURL):
+        return self.tryGetHomography(data_uri_to_cv2_img(testDataURL))
+
+
+    def drawMarker(self,bgr,corners):
+        assert len(corners)%4 == 0
+        nMarker = int(len(corners)/4)
+        for n in range(nMarker):
+            for i in range(4):
+                i1 = (i+3)%4
+                x0,y0 = corners[n*4 + i1]
+                x1,y1 = corners[n*4 + i]
+                cv2.line(bgr, (int(x0),int(y0)), (int(x1),int(y1)), (0, 255, 0), 2)
+
+    def drawPt(self,bgr,x,y):
+        cv2.circle(bgr, (x, y), 4, (0, 0, 255), -1)
+    
+    def getHomographyCv(self,vCn1,vCn2):
+        self.h, status = cv2.findHomography(np.array(vCn1),np.array(vCn2))
+
